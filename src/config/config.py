375e898436d0a2ac842cb32164d6ed96c3232381
nlebovits <nissim.lebovits@vanderbilt.edu>
2024-06-25 21:53:54 +0200

    add heat config data

--- src/config/config.py
+++ src/config/config.py
@@ -1,8 +1,11 @@
 import os
 
-GCP_PROJECT_NAME = os.environ.get("GOOGLE_CLOUD_PROJECT_NAME")
-GCP_KEY_PATH = os.environ.get("GOOGLE_CLOUD_KEY_PATH")
-GCP_API_KEY = os.environ.get("GOOGLE_CLOUD_API_KEY")
-GEE_KEY = os.environ.get("GOOGLE_EARTH_ENGINE_KEY")
+from dotenv import load_dotenv
+
+load_dotenv()
+GOOGLE_CLOUD_PROJECT = os.getenv("GOOGLE_CLOUD_PROJECT")
 
 HEAT_SCALE = 90
+HEAT_MODEL_ASSET_ID = "projects/gee-urban-clim/assets/heat-model-00"
+HEAT_INPUT_PROPERTIES = ["longitude", "latitude", "landcover", "elevation"]
+TRAINING_DATA_COUNTRIES = ["Costa Rica", "Netherlands"]

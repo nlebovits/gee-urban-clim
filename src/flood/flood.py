a91ab1e34dbafb5a61780bd44fdc4a1a2c048280
nlebovits <nissim.lebovits@vanderbilt.edu>
2024-06-26 08:49:18 +0200

    get first draft of flood module up

---
+++ src/flood/flood.py
@@ -0,0 +1,22 @@
+import argparse
+from .utils import make_training_data, train_and_evaluate, predict
+
+
+def main(place_name):
+    make_training_data()
+    train_and_evaluate()
+    print(f"Predicting for {place_name}...")
+    predict(place_name)
+    print(f"Prediction for {place_name} completed.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Run flood prediction pipeline for a given place."
+    )
+    parser.add_argument(
+        "place_name", type=str, help="The name of the place to predict on"
+    )
+
+    args = parser.parse_args()
+    main(args.place_name)
